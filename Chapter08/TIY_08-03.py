ceb65820e5c2ad27b2131606b43c8a9986550ccc
haroonattari <haroon.attari@gmail.com>
2017-04-17 12:10:47 +0500

    Started Chapter 8

---
+++ Chapter08/TIY_08-03.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    print("T-Shirt is ready with '{0}' size and message '{1}' on it".format(size, message))
+
+
+make_shirt("Large", "First solve the problem, then write the code")
+make_shirt(message="Python is fun", size="Small")
+make_shirt(size="Extra Large", message="Python is fun!")
---
+++ Chapter08/TIY_08-05.py
@@ -0,0 +1,6 @@
+def describe_city(city, country):
+    print("{0} is in {1}".format(city, country))
+
+
+describe_city("Karachi", "Paistan")
+
